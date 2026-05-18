ba0c90766b4c8ddbebf6aabbc3ea6c3fa308308b
michael.krasnovskyi <michael.krasnovskyi@gmail.com>
2026-05-18 19:18:16 +0300

    lesson3 code added

---
+++ lesson3/print.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.red('Hello world!'));
+console.log(chalk.white('Hello world!'));
+console.log(chalk.yellowBright('Hello world!'));
---
+++ lesson3/variables.js
@@ -0,0 +1,57 @@
+// // var age = 10;
+// let age = 10;
+// let name = 'John';
+// const city = 'New York';
+// let userMainAddress = 'Anonym';
+
+// // console.log(age);
+// // console.log(name);
+// // console.log(city);
+
+// // age = 'Hello';
+// // console.log(age);
+
+// console.log('Hello! My name is ' + name);
+// console.log('I am ' + age + ' years old');
+
+// Number
+// let age = 10;
+// let temperature = 10.5;
+
+// String
+// let name = 'Ivan';
+
+// Boolean
+// let isRaining = true;
+// let isUserLoggedIn = false;
+
+// Undefined
+// let undefinedVariable;
+// console.log(undefinedVariable);
+
+//NaN
+// let result = 'Hello' / 2;
+// console.log(result);
+
+// Infinity
+// let result = 10 / 0;
+// console.log(result);
+
+// BigInt
+// const age = 1234513214124122841412421257n;
+// console.log(age);
+
+// Object
+// let person = {
+//     name: 'John',
+//     age: 30,
+//     city: 'Kyiv'
+// }
+
+// Null
+// let noValue = null;
+
+
+
+
+// console.log(typeof isRaining);
